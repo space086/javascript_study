67daa826be29bb50860d10165503bd146ff9806c
space086 <space_086@naver.com>
2022-01-26 11:10:29 +0900

    main.js 수정

--- javascript/dino/main.js
+++ javascript/dino/main.js
@@ -8,28 +8,47 @@ canvas.height = window.innerHeight - 100;
 var dino = {
   x : 10,
   y : 200,
-  widrh: 50,
+  width: 50,
   height : 50,
   draw (){
     ctx.fillStyle = 'green'
-    ctx.fillRect(this.x, this.y, this,width, this.height);
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
-dino.draw()
+dino.x += 1;
 
 //장애물 그리기
 class Cactus {
   constructor(){
     this.x = 500;
-    this.y = 200;
-    this.width = 50;
-    this.height= 50;
+    this.y = 180;
+    this.width = 35;
+    this.height= 70;
   }
   draw(){
     ctx.fillStyle = 'red'
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
-var cactus = new Cactus();
-cactus.draw()
+var timer = 0;
+var cactusS = [];
 
+function frameRect(){
+  requestAnimationFrame(frameRect);
+  timer++;
+
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+
+  if(timer % 120 === 0) {
+    var cactus = new Cactus();
+    cactusS.push(cactus);
+  }
+  cactusS.forEach((a)=>{
+    a.x--;
+    a.draw();
+  })
+
+  dino.draw()
+}
+
+frameRect();
