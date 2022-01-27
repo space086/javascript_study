5980872370e28da633e8a26a72167f3a44751214
space086 <space_086@naver.com>
2022-01-27 20:15:10 +0900

    move

---
+++ fastcapus_study/0127/js_test/main.js
@@ -0,0 +1,15 @@
+import getType from './getType'
+
+console.log(typeof 'Hello world');
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined); //의도하지 않은 비어있는 값
+console.log(typeof null); //의도해서 비워놓은 값
+console.log(typeof {});
+console.log(typeof []);
+
+console.log(typeof(123));
+console.log(typeof(false));
+console.log(typeof(null));
+console.log(typeof{});
+console.log(typeof[]);
