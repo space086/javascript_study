3fa47a1d39d7dd24bb77b66aea122dc20c0c1044
space086 <space_086@naver.com>
2022-01-28 16:54:32 +0900

    js string

---
+++ fastcapus_study/0128/js_string/main.js
@@ -0,0 +1,31 @@
+
+//indexOf 결과값 6
+// const str = 'hello world!'
+
+// console.log(str.indexOf('world'))
+
+//boolean - false 만들기
+// const str = 'hello world!'
+// console.log(str.indexOf('heropy') !== -1)
+
+
+//slice  (결과값 world)
+// const str = "hellp world!"
+
+// console.log(str.slice(6,11))
+
+//replace
+// const str = 'hello world!'
+
+// console.log(str.replace('world' , 'heropy'))
+
+//match 정규표현식 사용
+// const str = 'space_086@naver.com!'
+
+// console.log(str.match(/.+(?=@)/)[0])
+
+//trim
+const str = '   hello world!    '
+
+console.log(str.trim())
+
