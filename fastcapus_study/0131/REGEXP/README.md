8f6163de9a601e4f898b93cc2d444ac183ec7846
space086 <space_086@naver.com>
2022-01-31 18:52:56 +0900

    readme 내용 추가 - 플래그

--- fastcapus_study/0131/REGEXP/README.md
+++ fastcapus_study/0131/REGEXP/README.md
@@ -24,3 +24,31 @@ new RegExp('[a-z]', 'gi')
 /[a-z]/gi
 ```
 
+## 예제 문자
+
+```js
+const str = `
+010-1234-5678
+thesecon@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+```
+
+## 메소드
+
+메소드 
+
+메소드 | 문법 | 설명
+--|--|--
+test| `정규식,test(문자열)` | 일치 여부 (Boolean) 반환
+match | `문자열,match(정규식)` | 일치하는 문자의 배열 (Array) 반환
+replace | `문자열, replace(정규식, 대체문자)` | 일치하는 문자를 대체 
+
+## 플래그 (옵션)
+
+플래그 | 설명
+g | 모든 문자 일치 (global)
+i | 영어 대소문자를 구분 않고 일치 (ignore case)
+m | 여러 줄 일치 (multi line)
--- fastcapus_study/0131/REGEXP/main.js
+++ fastcapus_study/0131/REGEXP/main.js
@@ -1,14 +1,25 @@
-const str = `
-010-1234-5678
+let str = `
+010-1234-5678.
 thesecon@gmail.com
 http://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `
 
-// g : the 를 검색하는 옵션
+// g : 모든 문자를 검색하는 옵션
 // i : 대소문자 가리지 않고 검색해주는 옵션 (gi)
 //const regexp = new RegExp('the', 'gi')
+ 
+// const regexp = /the/gi
+// console.log(regexp.test(str))
+// 원본 데이터 수정 x
+// console.log(str.replace(regexp, 'AAA'))
 
-const regexp = /the/gi
-console.log(str.match(regexp))
+// 원본 데이터에 변경된 값을 넣고 싶으면?
+//str 변수를 먼저 let으로 변경 (값 재할당 가능)
+// str = str.replace(regexp, 'AAA')
+// console.log(str)
+
+// \ = 이스케이프 문자 : 백슬래시 기호를 통해 본래의 기능에서 벗어나 상태가 바뀌는 문자
+console.log(str.match(/\.$/gi))
+
